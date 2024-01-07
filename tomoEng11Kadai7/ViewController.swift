8021c67c300afff57f2e357f0294aeed9f5c305c
tomo <tom_friendship@me.com>
2024-01-07 15:58:01 +0900

    Initial Commit

---
+++ tomoEng11Kadai7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tomoEng11Kadai7
+//
+//  Created by 井本智博 on 2024/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
